bd94ad24979dac9e6e0430aaa2ae45a5ee2bac46
Unicorn <49371158+Unicorn7141@users.noreply.github.com>
2022-09-01 19:25:22 +0300

    Update 2_📊_Results.py
    Signed-off-by: Unicorn <49371158+Unicorn7141@users.noreply.github.com>

--- pages/2_📊_Results.py
+++ pages/2_📊_Results.py
@@ -1,37 +1,14 @@
 import streamlit as st
 
-st.set_page_config(page_icon="📊")
+st.set_page_config(
+    page_title="Unicorn's \u2022 Results", 
+    page_icon="📊",
+    layout="wide",
+   initial_sidebar_state="expanded",
+)
 st.sidebar.success("Navigate to other pages here")
 st.markdown("""
 # Form Results
 ***
 Sadly I am yet to have any results to share with y'all 😥
 """)
-
-
-def set_page_title(title):
-    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
-        <iframe height=0 srcdoc="<script>
-            const title = window.parent.document.querySelector('title') \
-
-            const oldObserver = window.parent.titleObserver
-            if (oldObserver) {{
-                oldObserver.disconnect()
-            }} \
-
-            const newObserver = new MutationObserver(function(mutations) {{
-                const target = mutations[0].target
-                if (target.text !== '{title}') {{
-                    target.text = '{title}'
-                }}
-            }}) \
-
-            newObserver.observe(title, {{ childList: true }})
-            window.parent.titleObserver = newObserver \
-
-            title.text = '{title}'
-        </script>" />
-    """)
-
-
-set_page_title("Unicorn's \u2022 Results")
